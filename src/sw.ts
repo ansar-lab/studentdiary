244af75badf52bf04d04de8c11fb11f4f726c157
ansar <ansavali3231@gmail.com>
2025-10-18 11:59:11 +0530

    1

---
+++ src/sw.ts
@@ -0,0 +1,3 @@
+// This file is used by the vite-plugin-pwa and doesn't need custom code
+// The plugin will inject the service worker registration code
+// This empty file is required for the plugin to work correctly
